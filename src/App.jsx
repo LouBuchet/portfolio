368f765a2a27a95e382ea87b91e12acd0efcd4b9
LouBuchet <loujune.buchet@etu.unilim.fr>
2025-01-15 15:52:55 +0100

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -100,24 +100,24 @@ const App = () => {
 
   {/* PROJETS */}
 
-  <div className="mt-20 px-10 py-10 bg-bg rounded-lg flex flex-col space-y-8">
+  {/* <div className="mt-20 px-10 py-10 bg-bg rounded-lg flex flex-col space-y-8">
   <div className="flex flex-col md:w-1/2 font-druck">
     <h2 className="text-9xl font-bold text-rose">01.</h2>
     <h3 className="text-8xl font-semibold mt-2 text-rose">AFFICHES DE MATCHS</h3>
-  </div>
+  </div> */}
 
   {/* Contenu principal : texte descriptif et galerie d'images */}
-  <div className="flex flex-col md:flex-row gap-10"> 
+  {/* <div className="flex flex-col md:flex-row gap-10">  */}
     {/* Colonne gauche : Texte descriptif */}
-    <div className="w-full md:w-1/2 flex flex-col space-y-4">
+    {/* <div className="w-full md:w-1/2 flex flex-col space-y-4">
       <p className="text-lg font-light">GRAPHISME</p>
       <p className="text-lg leading-relaxed text-justify">
       Lors de mon stage au Limoges CSP, j'avais pour mission de réaliser les visuels de matchs pour l'équipe espoir. Ces affiches étaient destinées à promouvoir les rencontres sportives sur les réseaux sociaux et à maintenir l’engouement des supporters autour de cette catégorie de jeunes talents. Mon travail impliquait de créer des designs attractifs et dynamiques, en cohérence avec la charte graphique du club, tout en mettant en avant les informations essentielles du match. Cette responsabilité m’a permis de contribuer directement à la communication du club et de perfectionner mes compétences en graphisme, notamment sur des outils tels que Photoshop.
       </p>
-    </div>
+    </div> */}
 
     {/* Colonne droite : Galerie d'images */}
-    <div className="grid grid-cols-3 gap-4 w-full md:w-1/2 top-9">
+    {/* <div className="grid grid-cols-3 gap-4 w-full md:w-1/2 top-9">
       <div className="bg-gray-200 w-full h-40 rounded-lg"></div>
       <div className="bg-gray-200 w-full h-40 rounded-lg"></div>
       <div className="bg-gray-200 w-full h-40 rounded-lg"></div>
@@ -126,7 +126,7 @@ const App = () => {
       <div className="bg-gray-200 w-full h-40 rounded-lg"></div>
     </div>
   </div>
-</div>
+</div> */}
 
 
 
